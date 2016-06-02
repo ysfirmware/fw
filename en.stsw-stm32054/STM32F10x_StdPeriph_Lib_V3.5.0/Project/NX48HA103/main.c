713bb3038ca62160dadb5cd0ea27035e457d6a7b
KimYoungSaeng <zerosaeng2@nate.com>
2016-06-02 22:50:07 +0900

    TMP

--- en.stsw-stm32054/STM32F10x_StdPeriph_Lib_V3.5.0/Project/NX48HA103/main.c
+++ en.stsw-stm32054/STM32F10x_StdPeriph_Lib_V3.5.0/Project/NX48HA103/main.c
@@ -19,9 +19,125 @@
   ******************************************************************************
   */  
 
-void main()
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "InitMpu.h"
+#include "stdio.h"
+
+#include "motion.h"
+#include "AD7792.h"		    // AD7792 definitions.
+
+/** @addtogroup Template_Project
+  * @{
+  */ 
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static __IO uint32_t uwTimingDelay;
+RCC_ClocksTypeDef RCC_Clocks;
+
+/* Private function prototypes -----------------------------------------------*/
+
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
 {
+	SystemInit();   // setup STM32 system (clock, PLL and Flash configuration)
+	
+	/*!< At this stage the microcontroller clock setting is already configured, 
+	   this is done through SystemInit() function which is called from startup
+	   files before to branch to application main.
+	   To reconfigure the default setting of SystemInit() function, 
+	   refer to system_stm32f4xx.c file */
+
+	/* SysTick end of count event each 10ms */
+	RCC_GetClocksFreq(&RCC_Clocks);
+	SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);
+
+	/* Add your application code here */
+	Init_Mpu();
+	//iFlash_Init();
+	
+	
+	/* Insert 50 ms delay */
+	Delay(5);
+
+
+	/* SYSCLK/4 clock selected to output on MCO2 pin(PC9)*/
+	//RCC_MCO2Config(RCC_MCO2Source_SYSCLK, RCC_MCO2Div_4);
+	LED1_SET(1);
+	//LED2_SET(1);
+	Heater_Cont(HEATER_D,HEATER_ON);
+  //printf("start\n");
+
+	/* Infinite loop */
+	while (1)
+	{
+		//Motion_main();
+		//Comm_main();
+		
+		//printf("uu");
+		//Heater_main();
+		//Delay(250);
+
+		GPIO_ToggleBits(GPIOF, GPIO_Pin_3);
+
+		Delay(5);		
+	}
 }
 
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in milliseconds.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{ 
+  uwTimingDelay = nTime;
+
+  while(uwTimingDelay != 0);
+}
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (uwTimingDelay != 0x00)
+  { 
+    uwTimingDelay--;
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
 
 /******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
